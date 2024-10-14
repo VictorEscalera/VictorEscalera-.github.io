1219b4294ae93bfc59979f050ef3633d40a7410e
Victor Escalera <112970527+LuisCarlosNA@users.noreply.github.com>
2024-10-14 15:07:35 -0600

    APIS ULTIMATE

---
+++ apiposts.js
@@ -0,0 +1,23 @@
+/* DEFINIMOS LA RUTA DE DONDE CONSULTAREMOS LOS RECURSOS */
+const API_URL = 'https://jsonplaceholder.typicode.com';
+/* OBTENEMOS EL ELEMENTO DEL DOM HTML DONDE ARROJAREMOS LA INFO */
+const HTMLResponse = document.getElementById('appposts');
+/* CREAMOS EL ELEMENTO DONDE ARROJAREMOS LA INFO */
+const ol = document.createElement('ol');
+
+fetch(`${API_URL}/comments`)
+    .then((response) => response.json())
+    .then((comments) => {
+
+        const limitedComments = comments.slice(0, 10);
+        
+        limitedComments.forEach((comment) => {
+            let element = document.createElement('li');
+            element.innerHTML = `${comment.email}<br>${comment.body}`;
+            ol.appendChild(element);
+        });
+        HTMLResponse.appendChild(ol);
+    })
+    .catch((error) => {
+        console.error('Error en la solicitud:', error);
+    });
--- apiusers.js
+++ apiusers.js
@@ -1,28 +1,21 @@
-// definimos la rura dode consultaremos los recursos 
-const APIS_URL = 'https://jsonplaceholder.typicode.com/';
-// obtenemos el elemento del DOM html donde arrojamos la info
-const HTMLResponse = document.querySelector('#app');
-const ul = document.createElement('ul');
+document.addEventListener('DOMContentLoaded', () => {
+    const APIS_URL = 'https://jsonplaceholder.typicode.com/';
+    const HTMLResponse = document.querySelector('#app');
+    const ul = document.createElement('ul');
 
-fetch(`$(API_URL)/users`)
-.then((Response)=>Response.json())
-.then((users) => {
-    users.forEach((user) => {
-        // creamos ele elemento li para almacenar cada usuario en ul
-        let elem = document.createElement('li');
-        elem.appendChild(
-            document.createTextNode(`${user.name}`)
-        );
-        //agregamos el name dentro del li y dentro del ul
-        ul.appendChild(elem);
-
-    });
-    //agregamos el ul dentro del div obtenido
-    HTMLResponse.appendChild(ul)
-}).catch((error)=>{
-    console.error('error en la solicitud');
-}
-
-)
-
-    
+    fetch(`${APIS_URL}users`)  
+        .then((response) => response.json())
+        .then((users) => {
+            users.forEach((user) => {
+                let elem = document.createElement('li');
+                elem.appendChild(
+                    document.createTextNode(`${user.name}, ${user.phone}, ${user.email}`)
+                );
+                ul.appendChild(elem);
+            });
+            HTMLResponse.appendChild(ul);
+        })
+        .catch((error) => {
+            console.error('error en la solicitud', error);
+        });
+});
